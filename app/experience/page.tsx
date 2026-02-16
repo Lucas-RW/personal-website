12b7ec6d6e8888fa3e5e7f78ae41a9c0c8c60dba
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2026-02-15 22:32:07 -0500

    added background component + projects and experience routes

---
+++ app/experience/page.tsx
@@ -0,0 +1,5 @@
+export default function Experience() {
+  return (
+    <div>This is the Experience page.</div>
+  );
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,5 @@
+export default function Projects() {
+  return (
+    <div>This is the Projects page.</div>
+  );
+}
